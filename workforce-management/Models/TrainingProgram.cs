3151642fec4f466ad8ff883ebb3328270eb71f9d
KJ Miller <KJ Miller>
2017-08-18 10:42:21 -0500

    fixed training model

--- workforce-management/Models/TrainingProgram.cs
+++ workforce-management/Models/TrainingProgram.cs
@@ -1,11 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace workforceManagement.Models
 {
-    public class Class
+    public class TrainingProgram
     {
+        [Key]
+        public int TrainingProgramId { get; set; }
+        [Required]
+        public string TrainingProgramName { get; set; }
+        [Required]
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public string Description { get; set; }
+        public int MaxAttendees { get; set; }
+        public ICollection<TrainingPrgEmp> TrainingPrgEmp;
+
+
     }
 }
